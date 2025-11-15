a9ff1d3285a62e68648f0cfc455f6eca2f180447
Anatoly Neminushy <todin5005@gmail.com>
2025-11-15 13:48:49 +0300

    fix: set gh pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+const base = process.env.VITE_BASE_PATH ?? "/find_a_couple_game/";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base,
   plugins: [react(), tailwindcss()],
 });
